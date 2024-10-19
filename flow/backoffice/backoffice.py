ed0c244bb4775f38d9f7f07f766bda1760fc6589
sebas <sebastian-jadczak@wp.pl>
2024-10-19 11:35:46 +0200

    fix import path into MainSitePage

--- flow/backoffice/backoffice.py
+++ flow/backoffice/backoffice.py
@@ -1,5 +1,5 @@
 from flow.client.stiner import Stage
-from src.pages.stiner.main_site.main_site_page import MainSitePage
+from src.pages.stiner.main_site.page import MainSitePage
 
 
 class BackofficeStage(Stage):
