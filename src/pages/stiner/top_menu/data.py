99ef208f05a06eb8ab559e94a38543a5326fa3ca
sebas <sebastian-jadczak@wp.pl>
2024-10-20 15:45:42 +0200

    add data file in top_menu

---
+++ src/pages/stiner/top_menu/data.py
@@ -0,0 +1,13 @@
+from enum import StrEnum
+
+
+class ExploreSubmenu(StrEnum):
+    ATTRACTIONS = 'Atrakcje'
+    TRAILS = 'Trasy'
+    WORLD = 'Świat'
+
+
+class WorldSubmenu(StrEnum):
+    MALTA = 'Malta'
+    POLAND = 'Polska'
+    GERMANY = 'Niemcy'
