a738597cea7d272935e3bdcb81102c445f10c2bf
sebas <sebastian-jadczak@wp.pl>
2024-10-20 15:47:22 +0200

    add locators into trails

--- src/pages/stiner/trails/locators.py
+++ src/pages/stiner/trails/locators.py
@@ -2,6 +2,17 @@ from utils.locator import Locator
 
 
 class TrailsLocators:
-
     NAME_TRAILS_TEXTBOX = Locator.name('name')
-    POPULAR_TRAILS_TRUE_CHECKBOX = Locator.xpath('[@name="popular" and @value="True"]')
+    COUNTRY_SELECT = Locator.from_id('country')
+    REGION_SELECT = Locator.from_id('region')
+    CITY_SELECT = Locator.from_id('city')
+    TYPE_TRAIL_SELECT = Locator.from_id('type')
+    TOP_RATE_TRAIL_SELECT = Locator.from_id('top_rate')
+    POPULAR_TRAIL_SELECT = Locator.from_id('popular')
+    FILTER_BUTTON = Locator.from_id('button-filter')
+
+    # sort
+    SORTING_SELECT = Locator.from_id('sort-list')
+
+    def trail_link(self, trail_name: str) -> Locator:
+        return Locator.xpath(f'//p[@id="name-trail" and text=({trail_name})]')
