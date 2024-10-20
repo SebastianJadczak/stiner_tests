ff6da9bad05c48181dd2c1e58588a621ce0ef24d
sebas <sebastian-jadczak@wp.pl>
2024-10-20 16:03:20 +0200

    add ExploreStage class

---
+++ flow/client/explore_stage.py
@@ -0,0 +1,24 @@
+from flow.stage import Stage
+
+
+class ExploreStage(Stage):
+
+    @property
+    def attractions_page(self) -> ...:
+        return ...
+
+    @property
+    def trails_page(self) -> ...:
+        return ...
+
+    @property
+    def world_page(self) -> ...:
+        return ...
+
+    @property
+    def country_page(self) -> ...:
+        return ...
+
+    @property
+    def city_page(self) -> ...:
+        return ...
