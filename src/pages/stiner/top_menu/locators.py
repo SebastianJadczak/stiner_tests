c4bb02375e0262b28e243fa3f5894db70c1bfd4a
sebas <sebastian-jadczak@wp.pl>
2024-10-20 15:46:55 +0200

    add locators into top_menu

---
+++ src/pages/stiner/top_menu/locators.py
@@ -0,0 +1,19 @@
+from src.pages.stiner.top_menu.data import ExploreSubmenu, WorldSubmenu
+from utils.locator import Locator
+
+
+class MenuLocators:
+    LOGIN_REGISTER_BUTTON = Locator.from_id('button_login_in_first_menu')
+    EXPLORE_BUTTON = Locator.from_id('awarded_in_first_menu')
+    MAP_LINK = Locator.xpath('//a[text()="Mapa"]')
+    BLOG_LINK = Locator.xpath('//a[text()="Blog"]')
+    CONTACT_LINK = Locator.xpath('//a[text()="Kontakt"]')
+    EXPLORE_ELEMENT_LIST = Locator.from_id('sightseeing_in_second_menu')
+
+    # submenu
+
+    def submenu_explore_link(self, tab: ExploreSubmenu) -> Locator:
+        return Locator.xpath(f'//div[id="wrapper_sub_menu_sightseeing"]//a[text()="{tab}"]')
+
+    def submenu_world_link(self, tab: WorldSubmenu):
+        return Locator.xpath(f'//div[id="wrapper_sub_menu_sightseeing"]//a[text()="{tab}"]')
