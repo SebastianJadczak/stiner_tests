e3d5cb41d9a6e966b137f1b21e3543d0ad2a24af
sebas <sebastian-jadczak@wp.pl>
2024-10-20 15:48:02 +0200

    separating the tenant class into a separate file

--- src/pages/stiner/main_site/page.py
+++ src/pages/stiner/main_site/page.py
@@ -1,26 +1,21 @@
 import time
 
 from allure import step
-from allure import description, tag
 
 from src.pages.stiner.main_site.data import StinerData
-from utils.locator import Locator
+from src.pages.stiner.main_site.locators import LocatorsMainPage
 from utils.page import Page
-
-
-class _LocatorsMainPage:
-
-    NAME_INPUT = Locator.name('name')
-    NEWSLETTER_INPUT = Locator.from_id('email')
 
 
 class MainSitePage(Page):
     PAGE_NAME = 'Main Page'
 
-    @step(f'Wypełnij page {PAGE_NAME} i idź dalej.')
-    @description('123')
-    @tag('123')
-    def fill_and_go_next(self, data: StinerData):
+    @property
+    def locators(self) -> LocatorsMainPage:
+        return LocatorsMainPage()
+
+    @step(f'Wypełnij page {PAGE_NAME}.')
+    def fill(self, data: StinerData):
         self.get('https://sebastianjadczak.usermd.net')
         time.sleep(3)
-        self.send_keys(_LocatorsMainPage.NAME_INPUT, data.field_first)
+        self.send_keys(self.locators.NAME_INPUT, data.field_first)
