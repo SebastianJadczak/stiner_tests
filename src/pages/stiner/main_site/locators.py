dd029874b9bffb84818deb2398f932737b84e292
sebas <sebastian-jadczak@wp.pl>
2024-10-26 14:48:25 +0200

    rename class and add SIGN_ME_UP_BUTTON locator

--- src/pages/stiner/main_site/locators.py
+++ src/pages/stiner/main_site/locators.py
@@ -1,13 +1,14 @@
 from utils.locator import Locator
 
 
-class LocatorsMainPage:
+class LocatorsMainSite:
     NAME_INPUT = Locator.name('name')
     COUNTRY_SELECT = Locator.name('country')
     CITY_SELECT = Locator.name('city')
     NEWSLETTER_INPUT = Locator.from_id('email')
     POPULAR_TRAIL_RADIO = Locator.name('popular')
     SEARCH_BUTTON = Locator.xpath('//input[text()="Szukaj"]')
+    SIGN_ME_UP_BUTTON = Locator.xpath('//input[@value="Zapisz mnie"]')
     SEA_BUTTON = Locator.from_id('Morze')
     MOUNTAIN_BUTTON = Locator.from_id('Góry')
     LAKES_BUTTON = Locator.from_id('Jeziora')
