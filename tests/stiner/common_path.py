779836425a69394147c0378275a07bea5206b72f
sebas <sebastian-jadczak@wp.pl>
2024-10-26 14:45:19 +0200

    removal of unnecessary items in common path

--- tests/stiner/common_path.py
+++ tests/stiner/common_path.py
@@ -1,8 +1,5 @@
 from flow.stiner import StinerContainer
-from src.pages.stiner.main_site.data import StinerData
-from src.pages.stiner.main_site.page import MainSitePage
-from src.pages.stiner.trails.data import TrailsData
-from src.pages.stiner.trails.page import TrailsPage
+from src.pages.stiner.data import StinerData
 
 
 class CommonPath:
@@ -10,13 +7,9 @@ class CommonPath:
         self.driver = driver
         self.flow = StinerContainer(driver)
         self.client = self.flow.client
-        self.main_page = MainSitePage(self.driver)
-        self.trails_page = TrailsPage(self.driver)
 
     def test_fast_path(self, data: StinerData):
-        self.client.main_site_page.fill_and_go_next(data)
-        self.main_page.fill_and_go_next(data)
+        self.client.main_site_page.fill(data.main_page)
 
-    def test_trail_path(self, data: TrailsData):
-        self.trails_page.fill_and_go_next(data)
+
 
