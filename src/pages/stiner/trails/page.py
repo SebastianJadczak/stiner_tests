01f943edbc403c3ddec22d7f099f96ce8e3c4f8f
sebas <sebastian-jadczak@wp.pl>
2024-10-26 14:50:51 +0200

    add PAGE_NAME in trails

--- src/pages/stiner/trails/page.py
+++ src/pages/stiner/trails/page.py
@@ -1,9 +1,11 @@
+from src.pages.stiner.common.enums import PageName
 from src.pages.stiner.trails.data import TrailsData
 from src.pages.stiner.trails.locators import TrailsLocators as Locs
 from utils.page import Page
 
 
 class TrailsPage(Page):
+    PAGE_NAME = PageName.TRAILS
 
     def fill_and_go_next(self, data: TrailsData):
         self.get('https://sebastianjadczak.usermd.net/trails/all_trails')
