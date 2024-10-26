5c8a0a98e7dd7ccdc9e4ef490ebce7001c23cf02
sebas <sebastian-jadczak@wp.pl>
2024-10-26 14:50:19 +0200

    add fill methods

--- src/pages/stiner/main_site/page.py
+++ src/pages/stiner/main_site/page.py
@@ -1,21 +1,28 @@
-import time
-
 from allure import step
 
-from src.pages.stiner.main_site.data import StinerData
-from src.pages.stiner.main_site.locators import LocatorsMainPage
+from src.pages.stiner.common.enums import PageName
+from src.pages.stiner.main_site.data import MainSiteData, NewsletterData, TrailsData
+from src.pages.stiner.main_site.locators import LocatorsMainSite
 from utils.page import Page
 
 
 class MainSitePage(Page):
-    PAGE_NAME = 'Main Page'
+    PAGE_NAME = PageName.MAIN_SITE
 
     @property
-    def locators(self) -> LocatorsMainPage:
-        return LocatorsMainPage()
+    def locators(self) -> LocatorsMainSite:
+        return LocatorsMainSite()
 
     @step(f'Wypełnij page {PAGE_NAME}.')
-    def fill(self, data: StinerData):
-        self.get('https://sebastianjadczak.usermd.net')
-        time.sleep(3)
-        self.send_keys(self.locators.NAME_INPUT, data.field_first)
+    def fill(self, data: MainSiteData):
+        if data.newsletter:
+            self.__fill_newsletter_form(data.newsletter)
+
+    @step('Wypełnij formularz "Zapisz się do Newslettera"')
+    def __fill_newsletter_form(self, data: NewsletterData):
+        self.send_keys(self.locators.NEWSLETTER_INPUT, data.email)
+        self.click(self.locators.SIGN_ME_UP_BUTTON)
+
+    @step('Wypełnij formularz "Szukania trasy"')
+    def __fill_search_trail_form(self, data: TrailsData):
+        ...
