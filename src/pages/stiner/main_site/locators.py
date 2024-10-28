26a2073a4944efb1cab16c54c19dcd4be0889ad2
sebas <sebastian-jadczak@wp.pl>
2024-10-28 18:22:18 +0100

    rename locator in main_site

--- src/pages/stiner/main_site/locators.py
+++ src/pages/stiner/main_site/locators.py
@@ -2,7 +2,7 @@ from utils.locator import Locator
 
 
 class LocatorsMainSite:
-    NAME_INPUT = Locator.name('name')
+    NAME_TRAIL_INPUT = Locator.name('name')
     COUNTRY_SELECT = Locator.name('country')
     CITY_SELECT = Locator.name('city')
     NEWSLETTER_INPUT = Locator.from_id('email')
