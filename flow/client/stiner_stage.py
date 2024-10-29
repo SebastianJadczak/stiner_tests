9fe82df06369aa5adb755369b0230049b65667dc
sebas <sebastian-jadczak@wp.pl>
2024-10-29 12:56:30 +0100

    add property into stage

--- flow/client/stiner_stage.py
+++ flow/client/stiner_stage.py
@@ -1,6 +1,9 @@
 from flow.client.explore_stage import ExploreStage
 from flow.stage import Stage
+from src.pages.stiner.contact.page import ContactPage
+from src.pages.stiner.login.page import LoginPage
 from src.pages.stiner.main_site.page import MainSitePage
+from src.pages.stiner.register.page import RegisterPage
 
 
 class StinerStage(Stage):
@@ -17,12 +20,16 @@ class StinerStage(Stage):
         return ...
 
     @property
-    def contact_page(self) -> ...:
-        return ...
+    def contact_page(self) -> ContactPage:
+        return ContactPage(self.driver)
 
     @property
-    def register_page(self) -> ...:
-        return ...
+    def register_page(self) -> RegisterPage:
+        return RegisterPage(self.driver)
+
+    @property
+    def login_page(self) -> LoginPage:
+        return LoginPage(self.driver)
 
     @property
     def explore_stage(self) -> ExploreStage:
