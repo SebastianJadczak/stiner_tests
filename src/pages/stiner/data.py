1cba014cc40f3fe9045846288ce22f92aee4d4ce
sebas <sebastian-jadczak@wp.pl>
2024-10-29 12:53:52 +0100

    add new pages in StinerData dataclass

--- src/pages/stiner/data.py
+++ src/pages/stiner/data.py
@@ -1,7 +1,9 @@
 from dataclasses import dataclass
 
 from src.pages.stiner.contact.data import ContactData
+from src.pages.stiner.login.page import LoginPage
 from src.pages.stiner.main_site.data import MainSiteData
+from src.pages.stiner.register.page import RegisterPage
 from src.pages.stiner.trails.page import TrailsData
 
 
@@ -10,3 +12,5 @@ class StinerData:
     main_page: MainSiteData
     trails_page: TrailsData
     contact_page: ContactData
+    login_page: LoginPage
+    register_page: RegisterPage
