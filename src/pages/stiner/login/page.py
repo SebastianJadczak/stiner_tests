2abd15fde729b860261bc8f50bf5708cf9b3282c
sebas <sebastian-jadczak@wp.pl>
2024-10-29 12:55:13 +0100

    add methods in login page

--- src/pages/stiner/login/page.py
+++ src/pages/stiner/login/page.py
@@ -1,12 +1,28 @@
 from allure import step
 
+from src.pages.stiner.common.enums import PageName
 from src.pages.stiner.login.data import LoginData
+from src.pages.stiner.login.locators import LoginLocators
 from utils.page import Page
 
 
 class LoginPage(Page):
-    PAGE_NAME = ''
+    PAGE_NAME = PageName.LOGIN
 
-    @step(f'Wypełnij stronę {PAGE_NAME}')
+    @property
+    def locators(self) -> LoginLocators:
+        return LoginLocators()
+
+    @step(f'Wypełnij stronę {PAGE_NAME}.')
     def fill(self, data: LoginData):
-        ...
+        self.send_keys(self.locators.USERNAME_INPUT, data.username)
+        self.send_keys(self.locators.PASSWORD_INPUT, data.password)
+        self.click(self.locators.LOGIN_BUTTON)
+
+    @step('Otwórz stronę rejestracji.')
+    def open_register_page(self):
+        self.click(self.locators.REGISTER_LINK)
+
+    @step('Otwóz stronę przypomnienia hasła')
+    def open_forgotten_password_page(self):
+        self.click(self.locators.FORGOTTEN_PASSWORD_LINK)
