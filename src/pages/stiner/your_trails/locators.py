9f0527275b79408ff766f7a6332645d8851a5386
sebas <sebastian-jadczak@wp.pl>
2024-11-21 16:02:31 +0100

    add locator into your_trails

---
+++ src/pages/stiner/your_trails/locators.py
@@ -0,0 +1,17 @@
+from utils.locator import Locator
+
+
+class YourTrailsLocators:
+    CREATE_TRIP_LINK = Locator.from_id('create_trip')
+
+    def trail_link(self, name_trail: str):
+        return Locator.xpath(f'//p[text()="{name_trail}"]//..//..//..//..//a')
+
+
+class SearchYourTrailsLocators:
+    NAME_TRAIL_INPUT = Locator.from_id('name')
+    COUNTRY_TRAIL_SELECT = Locator.from_id('country')
+    CITY_TRAIL_SELECT = Locator.from_id('city')
+    TOP_RATE_TRAIL_SELECT = Locator.from_id('type')  # błąd w kodzie aplikacji
+    IS_POPULAR_TRAIL_SELECT = Locator.from_id('popular')
+    FILTER_BUTTON = Locator.from_id('button-filter')
