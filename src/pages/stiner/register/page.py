28f073482b78a6dc3edc7f006623e1ca774d3e3d
sebas <sebastian-jadczak@wp.pl>
2024-11-21 16:00:56 +0100

    fix access into property actions in RegisterPage

--- src/pages/stiner/register/page.py
+++ src/pages/stiner/register/page.py
@@ -1,9 +1,9 @@
 from allure import step
 
 from src.pages.stiner.common.enums import PageName
-from src.pages.stiner.register.data import RegisterData
+from src.pages.stiner.register.data import RegisterPageData
 from src.pages.stiner.register.locators import RegisterLocators
-from utils.page import Page
+from utils.application_model.page import Page
 
 
 class RegisterPage(Page):
@@ -14,9 +14,9 @@ class RegisterPage(Page):
         return RegisterLocators()
 
     @step(f'Wypełnij page {PAGE_NAME}')
-    def fill(self, data: RegisterData):
-        self.send_keys(self.locators.USERNAME_INPUT, data.username)
-        self.send_keys(self.locators.PASSWORD_INPUT, data.password)
-        self.send_keys(self.locators.MAIL_INPUT, data.email)
-        self.click(self.locators.REGULATIONS_CHECKBOX)
-        self.click(self.locators.REGISTER_BUTTON)
+    def fill(self, data: RegisterPageData):
+        self.actions.send_keys(self.locators.USERNAME_INPUT, data.username)
+        self.actions.send_keys(self.locators.PASSWORD_INPUT, data.password)
+        self.actions.send_keys(self.locators.MAIL_INPUT, data.email)
+        self.actions.click(self.locators.REGULATIONS_CHECKBOX)
+        self.actions.click(self.locators.REGISTER_BUTTON)
