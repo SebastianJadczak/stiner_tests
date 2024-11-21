f2c47b31b294b1a0e8589c4576be4cacc4e7c647
sebas <sebastian-jadczak@wp.pl>
2024-11-21 16:00:02 +0100

    fix access into property actions in ContactPage

--- src/pages/stiner/contact/page.py
+++ src/pages/stiner/contact/page.py
@@ -1,9 +1,9 @@
 from allure import step
 
 from src.pages.stiner.common.enums import PageName
-from src.pages.stiner.contact.data import ContactFormData, ContactData
+from src.pages.stiner.contact.data import ContactFormData, ContactPageData
 from src.pages.stiner.contact.locators import ContactLocators
-from utils.page import Page
+from utils.application_model.page import Page
 
 
 class ContactPage(Page):
@@ -14,12 +14,12 @@ class ContactPage(Page):
         return ContactLocators()
 
     @step(f'Wypełnij page {PAGE_NAME}')
-    def fill(self, data: ContactData):
+    def fill(self, data: ContactPageData):
         self.__fill_contact_form(data.contact_form)
 
     @step('Wypełnij formularz kontaktowy')
     def __fill_contact_form(self, data: ContactFormData):
-        self.send_keys(self.locators.NAME_INPUT, data.name)
-        self.send_keys(self.locators.EMAIL_INPUT, data.mail)
-        self.send_keys(self.locators.COMMENTS_TEXTAREA, data.comments)
-        self.click(self.locators.SEND_INPUT)
+        self.actions.send_keys(self.locators.NAME_INPUT, data.name)
+        self.actions.send_keys(self.locators.EMAIL_INPUT, data.mail)
+        self.actions.send_keys(self.locators.COMMENTS_TEXTAREA, data.comments)
+        self.actions.click(self.locators.SEND_INPUT)
