3ea401ef124d0929cd9c5ad374f31f6c9410078b
sebas <sebastian-jadczak@wp.pl>
2024-11-22 17:07:50 +0100

    add new properties

--- flow/client/stiner_stage.py
+++ flow/client/stiner_stage.py
@@ -4,6 +4,7 @@ from src.pages.stiner.contact.page import ContactPage
 from src.pages.stiner.login.page import LoginPage
 from src.pages.stiner.main_site.page import MainSitePage
 from src.pages.stiner.register.page import RegisterPage
+from src.pages.stiner.your_trails.page import YourTrailsPage
 
 
 class StinerStage(Stage):
@@ -34,3 +35,15 @@ class StinerStage(Stage):
     @property
     def explore_stage(self) -> ExploreStage:
         return ExploreStage(self.driver)
+
+    @property
+    def your_trail_page(self) -> YourTrailsPage:
+        return YourTrailsPage(self.driver)
+
+    @property
+    def create_your_trail_stage(self) -> ...:
+        return ...
+
+    @property
+    def favorite_stage(self) -> ...:
+        return ...
