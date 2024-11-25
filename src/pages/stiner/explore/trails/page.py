204690beb2f76fcae257b5a8f61fce321100834a
sebas <sebastian-jadczak@wp.pl>
2024-11-25 15:43:15 +0100

    add locators as property into trails

--- src/pages/stiner/explore/trails/page.py
+++ src/pages/stiner/explore/trails/page.py
@@ -1,14 +1,18 @@
 from allure import step
 
 from src.pages.stiner.common.enums import PageName
-from src.pages.stiner.trails.data import TrailsPageData
-from src.pages.stiner.trails.locators import TrailsLocators as Locs
+from src.pages.stiner.explore.trails.data import TrailsPageData
+from src.pages.stiner.explore.trails.locators import TrailsLocators
 from utils.application_model.page import Page
 
 
 class TrailsPage(Page):
     PAGE_NAME = PageName.TRAILS
 
-    @step(f'Wypełnmij stronę {PAGE_NAME}')
+    @property
+    def locators(self) -> TrailsLocators:
+        return TrailsLocators()
+
+    @step(f'Wypełnij stronę {PAGE_NAME}')
     def fill(self, data: TrailsPageData):
-        self.actions.send_keys(Locs.NAME_TRAILS_TEXTBOX, data.search.name_trails)
+        self.actions.send_keys(self.locators.NAME_TRAILS_TEXTBOX, data.search.name_trails)
