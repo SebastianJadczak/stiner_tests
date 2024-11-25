2a180706ac00005e920d9a2c429469d40eebebda
sebas <sebastian-jadczak@wp.pl>
2024-11-25 15:28:51 +0100

    add new properties into ExploreStage

--- flow/client/explore_stage.py
+++ flow/client/explore_stage.py
@@ -1,19 +1,22 @@
 from flow.stage import Stage
+from src.pages.stiner.explore.attractions.page import AttractionPage
+from src.pages.stiner.explore.trails.page import TrailsPage
+from src.pages.stiner.explore.world.page import WorldPage
 
 
 class ExploreStage(Stage):
 
     @property
-    def attractions_page(self) -> ...:
-        return ...
+    def attractions_page(self) -> AttractionPage:
+        return AttractionPage(self.driver)
 
     @property
-    def trails_page(self) -> ...:
-        return ...
+    def trails_page(self) -> TrailsPage:
+        return TrailsPage(self.driver)
 
     @property
-    def world_page(self) -> ...:
-        return ...
+    def world_page(self) -> WorldPage:
+        return WorldPage(self.driver)
 
     @property
     def country_page(self) -> ...:
