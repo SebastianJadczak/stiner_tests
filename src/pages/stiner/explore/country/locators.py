7f6db156e43aa0b8f3683c03024e4fd218800b63
sebas <sebastian-jadczak@wp.pl>
2025-01-17 12:25:01 +0100

    add new locators

--- src/pages/stiner/explore/country/locators.py
+++ src/pages/stiner/explore/country/locators.py
@@ -4,6 +4,12 @@ from utils.locator import Locator
 
 class CountryLocators:
     DOWNLOAD_GUIDE_BUTTON = Locator.from_id('file-country')
+    ENABLE_CURRENCY_NOTIFICATION_BUTTON = Locator.xpath(
+        '//div[@id="currencies_form"]//button[contains(text()="Włącz powiadomienie"])')
+    CURRENCY_DROPDOWN = Locator.from_id('currency_notification_form')
+    EXPECTED_VALUE_DROPDOWN = Locator.from_id('expected_value')
+    EXPECTED_COURSE_INPUT = Locator.from_id('expected_course')
+    SET_NOTIFICATION_SUBMIT = Locator.xpath('//input[@value="Ustaw"]')
 
     def action_add_button(self, action: ActionAddForButtons) -> Locator:
         return Locator.xpath(f'//button[text()=" {action}"]')
